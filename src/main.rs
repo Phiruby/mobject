c384caeb23e2b171cd8d88254a3b5107d5650183
Phiruby <vilohithrao@gmail.com>
2025-12-16 14:05:03 -0500

    demo: multiple objects

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     let triangle = Box::new(Triangle::default());
     let shape = Box::new(Rectangle::default());
     println!("{:?}", triangle.vertices2d());
-    let mobjects: Vec<Box<dyn Shape>> = vec![shape];
+    let mobjects: Vec<Box<dyn Shape>> = vec![triangle, shape];
     let scene = Scene::new(Some(mobjects));
     scene.main_loop();
 }
--- src/shapes.rs
+++ src/shapes.rs
@@ -98,19 +98,19 @@ impl Default for Rectangle {
         Self {
             vertices: [
                 Vertex2D {
-                    position: Vec2::new(-0.5, -0.5),
+                    position: Vec2::new(-0.25, -0.25),
                     color: Vec3::new(1.0, 0.0, 0.0),
                 },
                 Vertex2D {
-                    position: Vec2::new(0.5, -0.5),
+                    position: Vec2::new(0.25, -0.25),
                     color: Vec3::new(0.0, 1.0, 0.0),
                 },
                 Vertex2D {
-                    position: Vec2::new(0.5, 0.5),
+                    position: Vec2::new(0.25, 0.25),
                     color: Vec3::new(0.0, 0.0, 1.0),
                 },
                 Vertex2D {
-                    position: Vec2::new(-0.5, 0.5),
+                    position: Vec2::new(-0.25, 0.25),
                     color: Vec3::new(1.0, 1.0, 1.0),
                 },
             ],
