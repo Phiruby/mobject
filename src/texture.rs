bc16b54a2aab9b089663246350de18dc3a8f851e
Phiruby <vilohithrao@gmail.com>
2025-12-19 17:14:49 -0500

    refactor: texture image creation

--- src/texture.rs
+++ src/texture.rs
@@ -1,12 +1,12 @@
 use crate::buffers;
 use ash::Device;
 use ash::vk::{
-    self, Buffer, BufferUsageFlags, DeviceMemory, Extent2D, Extent3D, ImageCreateFlags,
-    ImageCreateInfo, MemoryAllocateInfo, MemoryMapFlags, MemoryPropertyFlags,
-    PhysicalDeviceMemoryProperties, StructureType,
+    self, Buffer, BufferUsageFlags, DeviceMemory, Extent2D, Extent3D, Format, Image,
+    ImageCreateFlags, ImageCreateInfo, ImageTiling, ImageUsageFlags, MemoryAllocateInfo,
+    MemoryMapFlags, MemoryPropertyFlags, PhysicalDeviceMemoryProperties, StructureType,
 };
 use image::{DynamicImage, ImageBuffer, ImageReader, Rgba32FImage};
-pub fn load_image(path: &str) -> Rgba32FImage {
+fn load_image(path: &str) -> Rgba32FImage {
     ImageReader::open(path)
         .unwrap()
         .decode()
@@ -16,11 +16,55 @@ pub fn load_image(path: &str) -> Rgba32FImage {
         .clone() // NOTE: forced to clone since returns a reference to rgba
 }
 
+fn create_image(
+    device: &Device,
+    width: u32,
+    height: u32,
+    format: Format,
+    tiling: ImageTiling,
+    image_usage: ImageUsageFlags,
+    properties: MemoryPropertyFlags,
+    physical_device_memory_properties: PhysicalDeviceMemoryProperties,
+) -> (Image, DeviceMemory) {
+    let image_info = ImageCreateInfo {
+        s_type: StructureType::IMAGE_CREATE_INFO,
+        image_type: vk::ImageType::TYPE_2D,
+        extent: Extent3D {
+            width,
+            height,
+            depth: 1,
+        },
+        mip_levels: 1,
+        format,
+        tiling,
+        initial_layout: vk::ImageLayout::UNDEFINED,
+        usage: image_usage,
+        samples: vk::SampleCountFlags::TYPE_1,
+        sharing_mode: vk::SharingMode::EXCLUSIVE,
+        ..Default::default()
+    };
+    let image = unsafe { device.create_image(&image_info, None) }.unwrap();
+    let image_mem_requirements = unsafe { device.get_image_memory_requirements(image) };
+    let alloc_info = MemoryAllocateInfo {
+        s_type: StructureType::MEMORY_ALLOCATE_INFO,
+        allocation_size: image_mem_requirements.size,
+        memory_type_index: buffers::find_memory_type(
+            physical_device_memory_properties,
+            image_mem_requirements.memory_type_bits,
+            properties,
+        ),
+        ..Default::default()
+    };
+    let image_memory = unsafe { device.allocate_memory(&alloc_info, None) }.unwrap();
+    unsafe { device.bind_image_memory(image, image_memory, 0) }.unwrap();
+    (image, image_memory)
+}
+
 pub fn create_texture_image(
     device: &Device,
     image_path: &str,
     physical_device_memory_properties: PhysicalDeviceMemoryProperties,
-) {
+) -> (Buffer, DeviceMemory, Image, DeviceMemory) {
     let pixels = load_image(image_path);
     let (width, height) = pixels.dimensions();
     let size = width * height * 4; // 4 bytes = 32 bits (RGBA f32)
@@ -40,37 +84,15 @@ pub fn create_texture_image(
         )
     };
     unsafe { device.unmap_memory(memory) };
-    let texture_image_create_info = ImageCreateInfo {
-        s_type: StructureType::IMAGE_CREATE_INFO,
-        image_type: vk::ImageType::TYPE_2D,
-        extent: Extent3D {
-            width,
-            height,
-            depth: 1,
-        },
-        mip_levels: 1,
-        array_layers: 1,
-        format: vk::Format::R8G8B8A8_SRGB,
-        tiling: vk::ImageTiling::OPTIMAL,
-        initial_layout: vk::ImageLayout::UNDEFINED,
-        usage: vk::ImageUsageFlags::TRANSFER_DST | vk::ImageUsageFlags::SAMPLED,
-        sharing_mode: vk::SharingMode::EXCLUSIVE,
-        samples: vk::SampleCountFlags::TYPE_1,
-        flags: ImageCreateFlags::empty(),
-        ..Default::default()
-    };
-    let image = unsafe { device.create_image(&texture_image_create_info, None) }.unwrap();
-    let mem_requirements = unsafe { device.get_image_memory_requirements(image) };
-    let memory_alloc_info = MemoryAllocateInfo {
-        s_type: StructureType::MEMORY_ALLOCATE_INFO,
-        allocation_size: mem_requirements.size,
-        memory_type_index: buffers::find_memory_type(
-            physical_device_memory_properties,
-            mem_requirements.memory_type_bits,
-            MemoryPropertyFlags::DEVICE_LOCAL,
-        ),
-        ..Default::default()
-    };
-    let texture_image_memory = unsafe { device.allocate_memory(&memory_alloc_info, None) }.unwrap();
-    unsafe { device.bind_image_memory(image, texture_image_memory, 0) };
+    let (image, image_memory) = create_image(
+        device,
+        width,
+        height,
+        vk::Format::R8G8B8A8_SRGB,
+        vk::ImageTiling::OPTIMAL,
+        ImageUsageFlags::TRANSFER_DST | vk::ImageUsageFlags::SAMPLED,
+        MemoryPropertyFlags::DEVICE_LOCAL,
+        physical_device_memory_properties,
+    );
+    (buffer, memory, image, image_memory)
 }
