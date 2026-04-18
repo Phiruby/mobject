d1ddff33f33df06c8e6e9da45bb3724f8d250dae
Phiruby <vilohithrao@gmail.com>
2026-04-18 15:55:53 -0400

    feat: basic ode logic

--- mobject/src/physics/ode.rs
+++ mobject/src/physics/ode.rs
@@ -1,6 +1,13 @@
 use nalgebra_glm::{Mat3x3, Vec3, Quat};
 struct OdeSolver ();
 
+struct StateDerivative {
+    velocity: Vec3,
+    rotation_prime: Mat3x3,
+    force: Vec3,
+    torque: Vec3
+}
+
 /// Represents the state of a physical object
 /// Most metrics are self-explanatory. But important clarifications:
 /// 1. inertia_body and inertia_body_inv are the inertia tensor of the object about its center of mass
@@ -22,18 +29,42 @@ struct PhysicalState {
     torque: Vec3
 }
 
-trait Differentiate {
-    fn differentiate(&self) -> Self;
+impl std::ops::Mul<f32> for StateDerivative {
+    type Output = StateDerivative;
+    fn mul(self, rhs: f32) -> Self::Output {
+        StateDerivative { velocity: self.velocity * rhs, rotation_prime: self.rotation_prime * rhs, force: self.force * rhs, torque: self.torque * rhs }
+    }
 }
 
-impl Differentiate for PhysicalState {
-    fn differentiate(&self) -> Self {
-        todo!()
+impl std::ops::Add<StateDerivative> for PhysicalState {
+    type Output = PhysicalState;
+    fn add(self, rhs: StateDerivative) -> Self::Output {
+        PhysicalState {
+            mass: self.mass,
+            intertia_body: self.intertia_body,
+            intertia_body_inv: self.intertia_body_inv,
+            orientation: self.orientation,
+            position: self.position,
+            momentum: self.momentum,
+            angular_momentum: self.angular_momentum,
+            inertia_inv: self.inertia_inv,
+            velocity: self.velocity + rhs.velocity,
+            angular_velocity: self.angular_velocity + rhs.rotation_prime * self.orientation,
+            force: self.force + rhs.force,
+            torque: self.torque + rhs.torque
+        }
+    }
+}
+
+impl PhysicalState {
+    fn differentiate(&self) -> StateDerivative {
+        StateDerivative { velocity: self.velocity, rotation_prime: Mat3x3::identity(), force: nalgebra_glm::make_vec3(&[0.0, 0.0, 0.0]), torque: nalgebra_glm::make_vec3(&[0.0, 0.0, 0.0]) }
     }
 }
 
 impl OdeSolver {
-    pub fn solve(&self) {
-
+    pub fn solve(&self, state: PhysicalState) -> PhysicalState {
+        let dydt = state.differentiate();
+        state + dydt * 0.01
     }
 }
